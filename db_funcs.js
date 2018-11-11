c84c89be3fdf0b8a7911193a7def3a7a7b4f9625
Thatcher Freeman <thatcher@gatech.edu>
2018-11-11 14:01:10 -0500

    Fixed key condition

--- db_funcs.js
+++ db_funcs.js
@@ -95,7 +95,7 @@ module.exports.getListeningHistory = (user_id, timestamp_low, timestamp_high) =>
             ":TSL": timestamp_low,
             ":TSH": timestamp_high
         },
-        KeyConditionExpression: "user_id = :UID AND listening_date >= :TSL AND listening_date <= :TSH",
+        KeyConditionExpression: "user_id = :UID AND listening_date BETWEEN :TSL AND :TSH",
         ProjectionExpression: "listening_date, track_name, track_duration, track_explicit",
         // TableName : process.env.LISTENING_HISTORY_TABLE_NAME
         TableName: "listening_history"
@@ -145,7 +145,7 @@ module.exports.getListeningHistoryMultipleUsers = (user_ids, timestamp_low, time
                 ":TSL": timestamp_low,
                 ":TSH": timestamp_high
             },
-            KeyConditionExpression: "user_id = :UID AND listening_date >= :TSL AND listening_date <= :TSH",
+            KeyConditionExpression: "user_id = :UID AND listening_date BETWEEN :TSL AND :TSH",
             ProjectionExpression: "listening_date, track_name, track_duration, track_explicit",
             // TableName : process.env.LISTENING_HISTORY_TABLE_NAME
             TableName: "listening_history"
